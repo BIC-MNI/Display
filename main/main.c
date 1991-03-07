54adf39d65e45821cdef5c9d60354a946cbde0b7
david <david>
1991-03-07 18:11:18 +0000

    *** empty log message ***

--- main/main.c
+++ main/main.c
@@ -1,21 +1,28 @@
 #include  <stdio.h>
+#include  <math.h>
 #include  <def_standard.h>
 #include  <def_graphics.h>
+#include  <def_display.h>
 
 main()
 {
     Status         status;
+    view_struct    view;
     render_struct  render;
     window_struct  window;
+    object_struct  *objects;
     Status         G_initialize();
     Status         G_create_window();
     Status         G_delete_window();
     Status         G_terminate();
     void           G_update_window();
-    void           define_view();
+    void           G_define_view();
+    void           create_view();
     void           define_lights();
     void           define_render();
     void           draw_triangles();
+    void           rotate_view();
+    void           create_objects();
 
     status = G_initialize();
 
@@ -26,15 +33,17 @@ main()
         PRINT_ERROR( "Shit\n" );
     }
 
-    define_view( &window );
+    create_view( &view );
+
+    G_define_view( &window, &view );
 
     define_lights( &window );
 
     define_render( &window, &render );
 
-    draw_triangles( &window, &render );
+    create_objects( &objects );
 
-    G_update_window( &window );
+    rotate_view( &window, &view, &render, objects );
 
     PRINT( "Hit return\n" );
 
@@ -47,9 +56,8 @@ main()
     return( (int) status );
 }
 
-private  void  draw_triangles( window, render )
-    window_struct  *window;
-    render_struct  *render;
+private  void  create_objects( objects )
+    object_struct  **objects;
 {
     static  Colour   colour  = { 1.0, 1.0, 1.0 };
     static  Surfprop surfprop  = { 1.0, 1.0, 1.0, {1.0,1.0,1.0}, 1.0, 1.0 };
@@ -60,40 +68,32 @@ private  void  draw_triangles( window, render )
                                    {0.0, 0.5, 1.0},
                                    {-0.5, 0.0, 1.0} };
     static  int      indices[] = { 0, 1, 2 };
-    triangles_struct triangles;
-    void             G_draw_triangles();
+    Status           status;
+    Status           create_triangles_object();
 
-    triangles.colour = colour;
-    triangles.surfprop = surfprop;
-    triangles.n_points = 3;
-    triangles.points = points;
-    triangles.normals = normals;
-    triangles.n_triangles = 1;
-    triangles.triangle_indices = indices;
-
-    G_draw_triangles( window, &triangles, render );
+/*
+    status = create_triangles_object( &colour, &surfprop, 3, points, normals,
+                                      1, indices, objects );   
+*/
 }
 
-private  void  define_view( window )
-    window_struct  *window;
+private  void  create_view( view )
+    view_struct    *view;
 {
-    view_struct   view;
     void          G_define_view();
 
-    view.perspective_flag = TRUE;
+    view->perspective_flag = TRUE;
 
-    fill_Point( view.origin, 0.5, 0.5, 1.0 );
-    fill_Vector( view.line_of_sight, 0.0, 0.0, -1.0 );
-    fill_Vector( view.horizontal, 1.0, 0.0, 0.0 );
-    fill_Vector( view.up, 0.0, 1.0, 0.0 );
-    view.front_distance = 0.5;
-    view.perspective_distance = 1.0;
-    view.back_distance = 1.5;
+    fill_Point( view->origin, 0.5, 0.5, 1.0 );
+    fill_Vector( view->line_of_sight, 0.0, 0.0, -1.0 );
+    fill_Vector( view->horizontal, 1.0, 0.0, 0.0 );
+    fill_Vector( view->up, 0.0, 1.0, 0.0 );
+    view->front_distance = 0.5;
+    view->perspective_distance = 1.0;
+    view->back_distance = 1.5;
 
-    view.window_width = 1.1;
-    view.window_height = 1.1;
-
-    G_define_view( window, &view );
+    view->window_width = 1.1;
+    view->window_height = 1.1;
 }
 
 private  void  define_lights( window )
@@ -128,4 +128,41 @@ private  void  define_render( window, render )
     render->master_light_switch = ON;
 
     G_set_render( window, render );
+}
+
+private  void  rotate_view( window, view, render, objects )
+    window_struct  *window;
+    view_struct    *view;
+    render_struct  *render;
+    object_struct  *objects;
+{
+    Real   angle, s, c;
+    Vector line_of_sight;
+    void   G_define_view();
+    void   G_update_window();
+    void   assign_view_direction();
+    void   display_objects();
+
+    angle = 0.0;
+
+    while( TRUE )
+    {
+        c = cos( (double) angle );
+        s = sin( (double) angle );
+
+        Point_z( view->origin ) = c;
+        Point_x( view->origin ) = 0.5 + s;
+
+        fill_Vector( line_of_sight, -s, 0.0, -c );
+
+        assign_view_direction( view, &line_of_sight, &view->up );
+
+        G_define_view( window, view );
+
+        display_objects( window, objects, render );
+
+        G_update_window( window );
+
+        angle += 0.05;
+    }
 }
