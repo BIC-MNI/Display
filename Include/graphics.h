df7dd39baab0b073a15b9df100af125944b076c0
david <david>
1991-03-14 09:58:26 +0000

    *** empty log message ***

--- Include/graphics.h
+++ Include/graphics.h
@@ -1,7 +1,8 @@
 #ifndef  DEF_GRAPHICS
 #define  DEF_GRAPHICS
 
-#include  <def_graphics_dependent.h>
+#include  <def_standard.h>
+#include  <def_geometry.h>
 
 #define  OFF  0
 #define  ON   1
@@ -16,6 +17,7 @@ typedef  struct
     Real        front_distance, back_distance;
     Real        perspective_distance;
     Real        window_width, window_height;
+    Transform   modeling_transform;
 } view_struct;
 
 typedef enum { AMBIENT_LIGHT,
@@ -26,6 +28,7 @@ typedef enum { AMBIENT_LIGHT,
 
 typedef  struct
 {
+    Boolean       state;
     light_types   light_type;
     Colour        colour;
     Vector        direction;
@@ -46,5 +49,66 @@ typedef  struct
     shading_types   shading_type;
     Boolean         master_light_switch;
 }  render_struct;
+
+typedef  enum  {
+                   NO_EVENT,
+                   KEYBOARD_EVENT,
+                   MOUSE_MOVEMENT_EVENT,
+                   LEFT_MOUSE_DOWN_EVENT,
+                   LEFT_MOUSE_UP_EVENT,
+                   MIDDLE_MOUSE_DOWN_EVENT,
+                   MIDDLE_MOUSE_UP_EVENT,
+                   RIGHT_MOUSE_DOWN_EVENT,
+                   RIGHT_MOUSE_UP_EVENT,
+                   NUM_EVENT_TYPES
+               } event_types;
+
+typedef  struct
+{
+    event_types   event_type;
+    Window_id     window_id;
+
+    union
+    {
+        char   key_pressed;
+        Point  mouse_position;
+    } event_data;
+
+} event_struct;
+
+typedef  struct
+{
+    int           head, tail;
+    event_struct  *events;
+} event_queue_struct;
+
+typedef  Status  (*event_function_type)();
+
+typedef  struct
+{
+    event_function_type   actions[(int) NUM_EVENT_TYPES];
+} action_table_struct;
+
+#include  <def_objects.h>
+
+typedef  struct
+{
+    window_struct        window;
+    Point                mouse_position;
+    Point                prev_mouse_position;
+    view_struct          view;
+    light_struct         lights[N_LIGHTS];
+    action_table_struct  action_table;
+
+    render_struct        render;
+    object_struct        *objects;
+
+    int                  update_required;
+} graphics_struct;
+
+#define  DECL_EVENT_FUNCTION( f )  Status   f()
+#define  DEF_EVENT_FUNCTION( f )   Status   f( graphics, event ) \
+                                                graphics_struct  *graphics; \
+                                                event_struct     *event;
 
 #endif
