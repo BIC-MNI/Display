6f4f7dd539377e793d68d8c470a7ce4cf8f8183f
david <david>
1991-04-29 16:01:20 +0000

    Initial revision

--- callbacks/object_ops.c
+++ callbacks/object_ops.c
@@ -3,256 +3,107 @@
 
 public  DEF_MENU_FUNCTION( reverse_normals )   /* ARGSUSED */
 {
-    object_struct   *current_object;
-    Boolean         get_current_object();
-    Status          status;
-    Status          reverse_object_normals();
-
-    if( get_current_object( graphics, &current_object ) )
-    {
-        status = reverse_object_normals( current_object );
-
-        graphics->update_required = TRUE;
-    }
-
-    return( status );
-}
-
-public  DEF_MENU_UPDATE(reverse_normals )   /* ARGSUSED */
-{
+    void  reverse_object_normals();
+    reverse_object_normals( graphics->model.objects );
+    graphics->update_required = TRUE;
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( advance_visible )     /* ARGSUSED */
 {
-    object_struct    *current_object;
-    Boolean          get_current_object();
-    void             advance_current_object();
-    void             rebuild_selected_list();
+    object_struct  *objects;
 
-    if( get_current_object( graphics, &current_object ) )
+    objects = graphics->model.objects;
+    if( objects != (object_struct *) 0 )
     {
-        current_object->visibility = OFF;
-
-        advance_current_object( graphics );
-
-        if( get_current_object( graphics, &current_object ) )
+        while( objects != (object_struct *) 0 && objects->visibility == OFF )
         {
-            current_object->visibility = ON;
+            objects = objects->next;
         }
 
-        rebuild_selected_list( graphics, menu_window );
+        if( objects == (object_struct *) 0 )
+        {
+            graphics->model.objects->visibility = ON;
+        }
+        else
+        {
+            objects->visibility = OFF;
+            if( objects->next == (object_struct *) 0 )
+            {
+                graphics->model.objects->visibility = ON;
+            }
+            else
+            {
+                objects->next->visibility = ON;
+            }
+        }
 
         graphics->update_required = TRUE;
     }
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(advance_visible )     /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( retreat_visible )     /* ARGSUSED */
 {
-    object_struct    *current_object;
-    Boolean          get_current_object();
-    void             retreat_current_object();
-    void             rebuild_selected_list();
+    object_struct  *objects;
 
-    if( get_current_object( graphics, &current_object ) )
+    objects = graphics->model.objects;
+    if( objects != (object_struct *) 0 )
     {
-        current_object->visibility = OFF;
-
-        retreat_current_object( graphics );
-
-        if( get_current_object( graphics, &current_object ) )
+        while( objects->next != (object_struct *) 0 &&
+            objects->next->visibility == OFF )
         {
-            current_object->visibility = ON;
+            objects = objects->next;
         }
 
-        rebuild_selected_list( graphics, menu_window );
+        if( objects->next == (object_struct *) 0 )
+        {
+            graphics->model.objects->visibility = OFF;
+            objects->visibility = ON;
+        }
+        else
+        {
+            objects->next->visibility = OFF;
+            objects->visibility = ON;
+        }
 
         graphics->update_required = TRUE;
     }
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(retreat_visible )     /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( make_all_invisible )     /* ARGSUSED */
 {
-    Status           status;
-    object_struct    *current_object;
-    Boolean          get_current_object();
-    void             rebuild_selected_list();
+    object_struct  *objects;
 
-    if( get_current_object( graphics, &current_object ) )
+    objects = graphics->model.objects;
+    
+    while( objects != (object_struct *) 0 )
     {
-        BEGIN_TRAVERSE_OBJECT( status, current_object )
-            OBJECT->visibility = FALSE;
-        END_TRAVERSE_OBJECT
-
-        rebuild_selected_list( graphics, menu_window );
-
-        graphics->update_required = TRUE;
+        objects->visibility = OFF;
+        objects = objects->next;
     }
 
-    return( status );
-}
+    graphics->update_required = TRUE;
 
-public  DEF_MENU_UPDATE(make_all_invisible )     /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( make_all_visible )     /* ARGSUSED */
 {
-    Status           status;
-    object_struct    *current_object;
-    Boolean          get_current_object();
-    void             rebuild_selected_list();
+    object_struct  *objects;
 
-    if( get_current_object( graphics, &current_object ) )
+    objects = graphics->model.objects;
+    
+    while( objects != (object_struct *) 0 )
     {
-        BEGIN_TRAVERSE_OBJECT( status, current_object )
-            OBJECT->visibility = TRUE;
-        END_TRAVERSE_OBJECT
-
-        rebuild_selected_list( graphics, menu_window );
-
-        graphics->update_required = TRUE;
+        objects->visibility = ON;
+        objects = objects->next;
     }
 
-    return( status );
-}
+    graphics->update_required = TRUE;
 
-public  DEF_MENU_UPDATE(make_all_visible )     /* ARGSUSED */
-{
-    return( OK );
-}
-
-public  DEF_MENU_FUNCTION( advance_selected )     /* ARGSUSED */
-{
-    void    advance_current_object();
-    void    rebuild_selected_list();
-
-    advance_current_object( graphics );
-
-    rebuild_selected_list( graphics, menu_window );
-
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(advance_selected )     /* ARGSUSED */
-{
-    return( OK );
-}
-
-public  DEF_MENU_FUNCTION( retreat_selected )     /* ARGSUSED */
-{
-    void    retreat_current_object();
-    void    rebuild_selected_list();
-
-    retreat_current_object( graphics );
-
-    rebuild_selected_list( graphics, menu_window );
-
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(retreat_selected )     /* ARGSUSED */
-{
-    return( OK );
-}
-
-public  DEF_MENU_FUNCTION( descend_selected )     /* ARGSUSED */
-{
-    Status    status;
-    Status    push_current_object();
-    void      rebuild_selected_list();
-
-    status = push_current_object( graphics );
-
-    rebuild_selected_list( graphics, menu_window );
-
-    return( status );
-}
-
-public  DEF_MENU_UPDATE(descend_selected )     /* ARGSUSED */
-{
-    return( OK );
-}
-
-public  DEF_MENU_FUNCTION( ascend_selected )     /* ARGSUSED */
-{
-    Status    status;
-    Status    pop_current_object();
-    void      rebuild_selected_list();
-
-    status = pop_current_object( graphics );
-
-    rebuild_selected_list( graphics, menu_window );
-
-    return( status );
-}
-
-public  DEF_MENU_UPDATE(ascend_selected )     /* ARGSUSED */
-{
-    return( OK );
-}
-
-public  DEF_MENU_FUNCTION( toggle_object_visibility )     /* ARGSUSED */
-{
-    object_struct    *current_object;
-    Boolean          get_current_object();
-    void             rebuild_selected_list();
-
-    if( get_current_object( graphics, &current_object ) )
-    {
-        current_object->visibility = !current_object->visibility;
-
-        rebuild_selected_list( graphics, menu_window );
-
-        graphics->update_required = TRUE;
-    }
-
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(toggle_object_visibility )     /* ARGSUSED */
-{
-    return( OK );
-}
-
-public  DEF_MENU_FUNCTION( create_model )     /* ARGSUSED */
-{
-    object_struct    *current_object;
-    Boolean          get_current_object();
-    void             rebuild_selected_list();
-    Status           status;
-    Status           create_model_after_current();
-
-    status = OK;
-
-    if( get_current_object( graphics, &current_object ) )
-    {
-        status = create_model_after_current( graphics );
-
-        rebuild_selected_list( graphics, menu_window );
-
-        graphics->update_required = TRUE;
-    }
-
-    return( status );
-}
-
-public  DEF_MENU_UPDATE(create_model )     /* ARGSUSED */
-{
     return( OK );
 }
--- callbacks/view_ops.c
+++ callbacks/view_ops.c
@@ -4,15 +4,15 @@
 
 public  DEF_MENU_FUNCTION( make_view_fit )      /* ARGSUSED */
 {
-    void          update_view();
-    void          fit_view_to_domain();
-    Point         min_limit, max_limit;
-    Boolean       get_range_of_object();
+    void     update_view();
+    void     fit_view_to_domain();
+    Point    min_limit, max_limit;
+    Boolean  get_range_of_objects();
 
-    if( get_range_of_object( graphics->models[THREED_MODEL],
-                              TRUE, &min_limit, &max_limit ) )
+    if( get_range_of_objects( graphics->model.objects, TRUE,
+                              &min_limit, &max_limit ) )
     {
-        fit_view_to_domain( &graphics->three_d.view, &min_limit, &max_limit );
+        fit_view_to_domain( &graphics->view, &min_limit, &max_limit );
 
         update_view( graphics );
 
@@ -22,154 +22,104 @@ public  DEF_MENU_FUNCTION( make_view_fit )      /* ARGSUSED */
     return( OK );
 }
 
-public  DEF_MENU_UPDATE(make_view_fit )      /* ARGSUSED */
-{
-    return( OK );
-}
-
 public  DEF_MENU_FUNCTION( reset_view )      /* ARGSUSED */
 {
     void  update_view();
     void  reset_view_parameters();
 
     reset_view_parameters( graphics,
-                           &Default_line_of_sight, &Default_horizontal );
+                           Default_line_of_sight_x,
+                           Default_line_of_sight_y,
+                           Default_line_of_sight_z );
 
     update_view( graphics );
 
     graphics->update_required = TRUE;
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(reset_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( top_view )      /* ARGSUSED */
 {
-    static  Vector   line_of_sight = { 0.0, 0.0, -1.0 };
-    static  Vector   horizontal = { 1.0, 0.0, 0.0 };
     void  update_view();
     void  reset_view_parameters();
 
-    reset_view_parameters( graphics, &line_of_sight, &horizontal );
+    reset_view_parameters( graphics, 0.0, 0.0, -1.0 );
 
     update_view( graphics );
 
     graphics->update_required = TRUE;
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(top_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( bottom_view )      /* ARGSUSED */
 {
-    static  Vector   line_of_sight = { 0.0, 0.0, 1.0 };
-    static  Vector   horizontal = { -1.0, 0.0, 0.0 };
     void  update_view();
     void  reset_view_parameters();
 
-    reset_view_parameters( graphics, &line_of_sight, &horizontal );
+    reset_view_parameters( graphics, 0.0, 0.0, 1.0 );
 
     update_view( graphics );
 
     graphics->update_required = TRUE;
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(bottom_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( front_view )      /* ARGSUSED */
 {
-    static  Vector   line_of_sight = { 0.0, -1.0, 0.0 };
-    static  Vector   horizontal = { 1.0, 0.0, 0.0 };
     void  update_view();
     void  reset_view_parameters();
 
-    reset_view_parameters( graphics, &line_of_sight, &horizontal );
+    reset_view_parameters( graphics, 0.0, -1.0, 0.0 );
 
     update_view( graphics );
 
     graphics->update_required = TRUE;
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(front_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( back_view )      /* ARGSUSED */
 {
-    static  Vector   line_of_sight = { 0.0, 1.0, 0.0 };
-    static  Vector   horizontal = { -1.0, 0.0, 0.0 };
     void  update_view();
     void  reset_view_parameters();
 
-    reset_view_parameters( graphics, &line_of_sight, &horizontal );
+    reset_view_parameters( graphics, 0.0, 1.0, 0.0 );
 
     update_view( graphics );
 
     graphics->update_required = TRUE;
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(back_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( left_view )      /* ARGSUSED */
 {
-    static  Vector   line_of_sight = { 1.0, 0.0, 0.0 };
-    static  Vector   horizontal = { 0.0, -1.0, 0.0 };
     void  update_view();
     void  reset_view_parameters();
 
-    reset_view_parameters( graphics, &line_of_sight, &horizontal );
+    reset_view_parameters( graphics, 1.0, 0.0, 0.0 );
 
     update_view( graphics );
 
     graphics->update_required = TRUE;
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(left_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
 public  DEF_MENU_FUNCTION( right_view )      /* ARGSUSED */
 {
-    static  Vector   line_of_sight = { -1.0, 0.0, 0.0 };
-    static  Vector   horizontal = { 0.0, 1.0, 0.0 };
     void  update_view();
     void  reset_view_parameters();
 
-    reset_view_parameters( graphics, &line_of_sight, &horizontal );
+    reset_view_parameters( graphics, -1.0, 0.0, 0.0 );
 
     update_view( graphics );
 
     graphics->update_required = TRUE;
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(right_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
@@ -177,24 +127,11 @@ public  DEF_MENU_FUNCTION( toggle_perspective )      /* ARGSUSED */
 {
     void  update_view();
 
-    graphics->three_d.view.perspective_flag =
-         !graphics->three_d.view.perspective_flag;
+    graphics->view.perspective_flag = !graphics->view.perspective_flag;
 
     update_view( graphics );
 
     graphics->update_required = TRUE;
-
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(toggle_perspective )      /* ARGSUSED */
-{
-    void  set_text_boolean();
-
-    set_text_boolean( format, text, graphics->three_d.view.perspective_flag,
-                      "Parallel", "Perspective" );
-
-    menu_window->update_required = TRUE;
 
     return( OK );
 }
@@ -204,22 +141,12 @@ public  DEF_MENU_FUNCTION( centre_view )      /* ARGSUSED */
     return( OK );
 }
 
-public  DEF_MENU_UPDATE(centre_view )      /* ARGSUSED */
-{
-    return( OK );
-}
-
 public  DEF_MENU_FUNCTION( magnify_view )      /* ARGSUSED */
 {
     void  initialize_magnification();
 
-    initialize_magnification( graphics );
+    initialize_magnification( &graphics->action_table );
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(magnify_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
@@ -227,13 +154,8 @@ public  DEF_MENU_FUNCTION( translate_view )      /* ARGSUSED */
 {
     void  initialize_translation();
 
-    initialize_translation( graphics );
+    initialize_translation( &graphics->action_table );
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(translate_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
@@ -241,13 +163,8 @@ public  DEF_MENU_FUNCTION( rotate_view )      /* ARGSUSED */
 {
     void  initialize_virtual_spaceball();
 
-    initialize_virtual_spaceball( graphics );
+    initialize_virtual_spaceball( &graphics->action_table );
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(rotate_view )      /* ARGSUSED */
-{
     return( OK );
 }
 
@@ -255,13 +172,8 @@ public  DEF_MENU_FUNCTION( front_clipping )      /* ARGSUSED */
 {
     void  initialize_front_clipping();
 
-    initialize_front_clipping( graphics );
+    initialize_front_clipping( &graphics->action_table );
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(front_clipping )      /* ARGSUSED */
-{
     return( OK );
 }
 
@@ -269,26 +181,7 @@ public  DEF_MENU_FUNCTION( back_clipping )      /* ARGSUSED */
 {
     void  initialize_back_clipping();
 
-    initialize_back_clipping( graphics );
+    initialize_back_clipping( &graphics->action_table );
 
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(back_clipping )      /* ARGSUSED */
-{
-    return( OK );
-}
-
-public  DEF_MENU_FUNCTION( pick_view_rectangle )      /* ARGSUSED */
-{
-    void  start_picking_viewport();
-
-    start_picking_viewport( graphics );
-
-    return( OK );
-}
-
-public  DEF_MENU_UPDATE(pick_view_rectangle )      /* ARGSUSED */
-{
     return( OK );
 }
