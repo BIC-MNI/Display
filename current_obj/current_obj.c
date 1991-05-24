2eeb5974f5996d6a02ebb0e8bb3ba5c68995bb65
david <david>
1991-05-24 15:51:31 +0000

    Initial revision

--- current_obj/current_obj.c
+++ current_obj/current_obj.c
@@ -1,199 +1,51 @@
 
 #include  <def_graphics.h>
 
-public  void  advance_current_object( graphics )
-    graphics_struct   *graphics;
-{
-    int             object_index;
-    model_struct    *model;
-
-    if( !current_object_is_top_level(graphics) )
-    {
-        object_index = TOP_OF_STACK( graphics->current_object ).object_index;
-        model = TOP_OF_STACK(graphics->current_object).model_object->ptr.model;
-
-        if( model->n_objects > 0 )
-        {
-            TOP_OF_STACK( graphics->current_object ).object_index =
-                 (object_index + 1) % model->n_objects;
-        }
-    }
-}
-
-public  void  retreat_current_object( graphics )
-    graphics_struct   *graphics;
-{
-    int            object_index;
-    model_struct   *model;
-
-    if( !current_object_is_top_level(graphics) )
-    {
-        object_index = TOP_OF_STACK( graphics->current_object ).object_index;
-        model = TOP_OF_STACK(graphics->current_object).model_object->ptr.model;
-
-        if( model->n_objects > 0 )
-        {
-            TOP_OF_STACK( graphics->current_object ).object_index =
-                 (object_index - 1 + model->n_objects) % model->n_objects;
-        }
-    }
-}
-
-public  object_struct  *get_current_model_object( graphics )
-    graphics_struct   *graphics;
-{
-    object_struct   *model_object;
-
-    if( current_object_is_top_level(graphics) )
-    {
-        model_object = graphics->models[THREED_MODEL];
-    }
-    else
-    {
-        model_object = TOP_OF_STACK(graphics->current_object).model_object;
-    }
-
-    return( model_object );
-}
-
-public  model_struct  *get_current_model( graphics )
-    graphics_struct   *graphics;
-{
-    object_struct  *model_object;
-    object_struct  *get_current_model_object();
-    model_struct   *model;
-
-    model_object = get_current_model_object( graphics );
-
-    model = model_object->ptr.model;
-
-    return( model );
-}
-
-public  int  get_current_object_index( graphics )
-    graphics_struct   *graphics;
-{
-    int  index;
-
-    if( current_object_is_top_level(graphics) )
-    {
-        index = -1;
-    }
-    else
-    {
-        index = TOP_OF_STACK( graphics->current_object ).object_index;
-    }
-
-    return( index );
-}
-
-public  void  set_current_object_index( graphics, index )
-    graphics_struct   *graphics;
-    int               index;
-{
-    if( !current_object_is_top_level(graphics) )
-    {
-        if( index >= 0 &&
-            index < TOP_OF_STACK( graphics->current_object ).
-                            model_object->ptr.model->n_objects )
-        {
-            TOP_OF_STACK( graphics->current_object ).object_index = index;
-        }
-        else
-        {
-            HANDLE_INTERNAL_ERROR( "set current object index" );
-        }
-    }
-}
-
-public  Boolean  get_current_object( graphics, current_object )
-    graphics_struct   *graphics;
-    object_struct     **current_object;
-{
-    int             object_index;
-    Boolean         current_object_exists;
-    model_struct    *model;
-
-    if( current_object_is_top_level(graphics) )
-    {
-        *current_object = graphics->models[THREED_MODEL];
-        current_object_exists = TRUE;
-    }
-    else
-    {
-        object_index = TOP_OF_STACK( graphics->current_object ).object_index;
-        model = TOP_OF_STACK(graphics->current_object).model_object->ptr.model;
-
-        if( object_index >= 0 && object_index < model->n_objects )
-        {
-            *current_object = model->object_list[object_index];
-            current_object_exists = TRUE;
-        }
-        else
-        {
-            *current_object = (object_struct *) 0;
-            current_object_exists = FALSE;
-        }
-    }
-
-    return( current_object_exists );
-}
-
-public  Status  initialize_current_object( graphics )
-    graphics_struct   *graphics;
-{
-    INITIALIZE_STACK( graphics->current_object );
-
-    return( OK );
-}
-
-public  Status  terminate_current_object( graphics )
-    graphics_struct   *graphics;
-{
-    Status  status;
-
-    DELETE_STACK( status, graphics->current_object );
-
-    return( status );
-}
-
-public  Status  push_current_object( graphics )
-    graphics_struct   *graphics;
+public  Status  push_current_object( current_object, object_index, model )
+    selection_struct   *current_object;
+    int                object_index;
+    model_struct       *model;
 {
     Status            status;
     selection_entry   entry;
-    object_struct     *current_object;
 
-    status = OK;
+    entry.object_index = object_index;
+    entry.model = model;
 
-    if( get_current_object( graphics, &current_object ) &&
-        current_object->object_type == MODEL )
-    {
-        entry.object_index = 0;
-        entry.model_object = current_object;
-
-        PUSH_STACK( status, graphics->current_object, selection_entry,
-                    entry );
-    }
+    PUSH_STACK( status, *current_object, selection_entry, entry );
 
     return( status );
 }
 
-public  Boolean  current_object_is_top_level( graphics )
-    graphics_struct   *graphics;
+public  Boolean  current_object_is_top_level( current_object )
+    selection_struct   *current_object;
 {
-    return( IS_STACK_EMPTY(graphics->current_object) );
+    return( N_ENTRIES_IN_STACK(*current_object) == 1 );
 }
 
-public  Status  pop_current_object( graphics )
-    graphics_struct   *graphics;
+public  Status  pop_current_object( current_object, object_index, model )
+    selection_struct   *current_object;
+    int                *object_index;
+    model_struct       **model;
 {
+    Status            status;
     selection_entry   entry;
 
-    if( !current_object_is_top_level(graphics) )
+    if( N_ENTRIES_IN_STACK(*current_object) > 1 )
     {
-        POP_STACK( graphics->current_object, entry );
+        POP_STACK( status, *current_object, entry );
+
+        if( status == OK )
+        {
+            *object_index = entry.object_index;
+            *model = entry.model;
+        }
+    }
+    else
+    {
+        PRINT_ERROR( "pop current object" );
+        status = ERROR;
     }
 
-    return( OK );
+    return( status );
 }
