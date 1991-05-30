95115e4e6750bd9ef530d0769c1ed7b1c5ff8ee0
david <david>
1991-05-30 13:12:39 +0000

    *** empty log message ***

--- main/main.c
+++ main/main.c
@@ -12,12 +12,11 @@ int  main( argc, argv )
     Status           G_initialize();
     Status           initialize_globals();
     Status           initialize_menu();
-    Status           input_graphics_file();
+    Status           load_graphics_file();
     Status           create_graphics_window();
     Status           delete_graphics_window();
     Status           main_event_loop();
     Status           G_terminate();
-    Status           make_all_invisible();
     void             reset_view_parameters();
     void             update_view();
     void             set_model_scale();
@@ -26,12 +25,6 @@ int  main( argc, argv )
     model_struct     *get_graphics_model();
     Boolean          get_range_of_object();
     void             rebuild_selected_list();
-
-    if( argc != 2 )
-    {
-        PRINT_ERROR( "Argument.\n" );
-        (void) abort();
-    }
 
     status = initialize_globals();
 
@@ -76,49 +69,28 @@ int  main( argc, argv )
 
     if( status == OK )
     {
-        PRINT( "Inputting objects.\n" );
-
-        model = get_graphics_model( graphics, THREED_MODEL );
-
-        status = input_graphics_file( argv[1],
-                                      &model->n_objects,
-                                      &model->object_list );
-
-        graphics->update_required = TRUE;
-
-        PRINT( "Objects input.\n" );
-    }
-
-    if( status == OK )
-    {
-        if( !Visibility_on_input )
-        {
-            status = make_all_invisible( graphics, menu,
-                                         (menu_entry_struct *) 0);
-        }
-    }
-
-    if( status == OK )
-    {
         rebuild_selected_list( graphics, menu );
     }
 
     if( status == OK )
     {
-        if( !get_range_of_object( graphics->models[THREED_MODEL], FALSE,
-                                  &graphics->min_limit, &graphics->max_limit ) )
-        {
-            fill_Point( graphics->min_limit, 0.0, 0.0, 0.0 );
-            fill_Point( graphics->max_limit, 1.0, 1.0, 1.0 );
-            PRINT( "No objects range.\n" );
-        }
+        fill_Point( graphics->min_limit, 0.0, 0.0, 0.0 );
+        fill_Point( graphics->max_limit, 1.0, 1.0, 1.0 );
 
         ADD_POINTS( graphics->centre_of_objects, graphics->min_limit,
                     graphics->max_limit );
         SCALE_POINT( graphics->centre_of_objects,
                      graphics->centre_of_objects,
                      0.5 );
+    }
 
+    if( status == OK && argc > 1 )
+    {
+        status = load_graphics_file( graphics, argv[1] );
+    }
+
+    if( status == OK )
+    {
         reset_view_parameters( graphics, &Default_line_of_sight,
                                &Default_horizontal );
 
