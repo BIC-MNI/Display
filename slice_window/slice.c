2082dc7663bb5b14fb18e1048cc8a3a66138d901
david <david>
1991-06-11 14:39:16 +0000

    *** empty log message ***

--- slice_window/slice.c
+++ slice_window/slice.c
@@ -8,6 +8,7 @@ public  void  initialize_slice_window( graphics )
     DECL_EVENT_FUNCTION( window_size_changed );
     void                 add_action_table_function();
     void                 update_window_size();
+    int                  c;
 
     graphics->slice.volume = (volume_struct *) 0;
 
@@ -15,6 +16,15 @@ public  void  initialize_slice_window( graphics )
 
     add_action_table_function( &graphics->action_table, WINDOW_RESIZE_EVENT,
                                window_size_changed );
+
+    for_less( c, 0, N_DIMENSIONS )
+    {
+        graphics->slice.slice_views[c].slice_index = 0;
+        graphics->slice.slice_views[c].x_offset = 0;
+        graphics->slice.slice_views[c].y_offset = 0;
+        graphics->slice.slice_views[c].x_scale = 1.0;
+        graphics->slice.slice_views[c].y_scale = 1.0;
+    }
 }
 
 private  void  update_window_size( graphics )
@@ -39,7 +49,9 @@ private  DEF_EVENT_FUNCTION( window_size_changed )    /* ARGSUSED */
 public  void  update_slice_window( graphics )
     graphics_struct  *graphics;
 {
+    int   x_min, x_max, y_min, y_max;
     void  draw_2d_line();
+    void  draw_slice();
 
     draw_2d_line( graphics, PIXEL_VIEW, &Slice_split_colour,
                   (Real) graphics->slice.x_split, 0.0,
@@ -50,6 +62,27 @@ public  void  update_slice_window( graphics )
                   0.0, (Real) graphics->slice.y_split,
                   (Real) (graphics->window.x_size-1),
                   (Real) graphics->slice.y_split );
+
+    x_min = 0;
+    x_max = graphics->slice.x_split-2;
+    y_min = graphics->slice.y_split+2;
+    y_max = graphics->window.y_size-1;
+
+    draw_slice( graphics, X_AXIS, x_min, x_max, y_min, y_max );
+
+    x_min = graphics->slice.x_split+2;
+    x_max = graphics->window.x_size-1;
+    y_min = graphics->slice.y_split+2;
+    y_max = graphics->window.y_size-1;
+
+    draw_slice( graphics, Y_AXIS, x_min, x_max, y_min, y_max );
+
+    x_min = 0;
+    x_max = graphics->slice.x_split-2;
+    y_min = 0;
+    y_max = graphics->slice.y_split-2;
+
+    draw_slice( graphics, Z_AXIS, x_min, x_max, y_min, y_max );
 }
 
 public  Status  delete_slice_window_info( slice_window )
