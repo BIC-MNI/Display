efba96c0d394cb573649238708d293fdd837b971
david <david>
1991-12-09 15:55:51 +0000

    Initial revision

---
+++ callbacks/call_globals.c
@@ -0,0 +1,37 @@
+
+#include  <def_graphics.h>
+
+public  DEF_MENU_FUNCTION( menu_set_global_variable )  /* ARGSUSED */
+{
+    Status   status;
+    Status   input_string();
+    String   input_str, variable_name, new_value;
+    Status   set_or_get_global_variable();
+
+    (void) printf( "variable_name [= value]: " );
+
+    status = input_string( stdin, input_str, '\n' );
+
+    if( status == OK )
+    {
+        status = set_or_get_global_variable( input_str, variable_name,
+                                             new_value );
+    }
+
+    if( status == OK )
+    {
+        PRINT( "%s = %s\n", variable_name, new_value );
+    }
+
+    if( status != OK )
+    {
+        PRINT_ERROR( "Error in setting global variable\n" );
+    }
+
+    return( status );
+}
+
+public  DEF_MENU_UPDATE(menu_set_global_variable )  /* ARGSUSED */
+{
+    return( OK );
+}
