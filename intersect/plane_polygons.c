7d6f13c6d9503e988ce2750b95cb23e400e715c2
david <david>
1992-05-07 11:00:15 +0000

    Initial revision

---
+++ intersect/plane_polygons.c
@@ -0,0 +1,121 @@
+
+#include  <def_graphics.h>
+
+public  Status  intersect_plane_with_polygons( graphics, plane_normal,
+                                               plane_constant, lines,
+                                               n_points_alloced,
+                                               n_indices_alloced,
+                                               n_end_indices_alloced )
+    graphics_struct   *graphics;
+    Vector            *plane_normal;
+    Real              plane_constant;
+    lines_struct      *lines;
+    int               *n_points_alloced;
+    int               *n_indices_alloced;
+    int               *n_end_indices_alloced;
+{
+    Status         status;
+    int            i;
+    int            model_index;
+    Status         intersect_plane_polygons();
+    object_struct  *object;
+
+    status = OK;
+
+    lines->n_items = 0;
+    lines->n_points = 0;
+
+    for_less( model_index, 0, N_MODELS )
+    {
+        for_less( i, 0, graphics->models[model_index]->ptr.model->n_objects )
+        {
+            object = graphics->models[model_index]->ptr.model->object_list[i];
+
+            BEGIN_TRAVERSE_OBJECT( status, object )
+
+                if( OBJECT->object_type == POLYGONS && OBJECT->visibility )
+                {
+                    status = intersect_plane_polygons( plane_normal,
+                                              plane_constant,
+                                              OBJECT->ptr.polygons,
+                                              lines,
+                                              n_points_alloced,
+                                              n_indices_alloced,
+                                              n_end_indices_alloced );
+
+                    if( status != OK ) break;
+                }
+
+            END_TRAVERSE_OBJECT
+
+            if( status != OK ) break;
+        }
+        if( status != OK ) break;
+    }
+
+    return( status );
+}
+
+private  Status  intersect_plane_polygons( plane_normal, plane_constant,
+                                           polygons, lines,
+                                           n_points_alloced,
+                                           n_indices_alloced,
+                                           n_end_indices_alloced )
+    Vector            *plane_normal;
+    Real              plane_constant;
+    polygons_struct   *polygons;
+    lines_struct      *lines;
+    int               *n_points_alloced;
+    int               *n_indices_alloced;
+    int               *n_end_indices_alloced;
+{
+    Status    status;
+    int       i, point_index, n_indices;
+    Point     points[2];
+    Boolean   get_plane_polygon_intersection();
+
+    status = OK;
+
+    if( FALSE && polygons->bintree != (bintree_struct *) 0 )
+    {
+/*
+        intersect_ray_with_bintree( ray_origin, ray_direction,
+                                                 polygons->bintree, polygons,
+                                                 poly_index, dist );
+*/
+    }
+    else
+    {
+        for_less( i, 0, polygons->n_items )
+        {
+            if( get_plane_polygon_intersection( plane_normal, plane_constant,
+                                                polygons, i, points ) )
+            {
+                point_index = lines->n_points;
+
+                ADD_ELEMENT_TO_ARRAY_WITH_SIZE( status,
+                         *n_points_alloced, lines->n_points, lines->points,
+                         points[0], Point, DEFAULT_CHUNK_SIZE );
+                ADD_ELEMENT_TO_ARRAY_WITH_SIZE( status,
+                         *n_points_alloced, lines->n_points, lines->points,
+                         points[1], Point, DEFAULT_CHUNK_SIZE );
+
+                n_indices = NUMBER_INDICES( *lines );
+
+                ADD_ELEMENT_TO_ARRAY_WITH_SIZE( status,
+                         *n_indices_alloced, n_indices, lines->indices,
+                         point_index, int, DEFAULT_CHUNK_SIZE );
+                ADD_ELEMENT_TO_ARRAY_WITH_SIZE( status,
+                         *n_indices_alloced, n_indices, lines->indices,
+                         point_index+1, int, DEFAULT_CHUNK_SIZE );
+
+                ADD_ELEMENT_TO_ARRAY_WITH_SIZE( status,
+                         *n_end_indices_alloced, lines->n_items,
+                         lines->end_indices,
+                         n_indices, int, DEFAULT_CHUNK_SIZE );
+            }
+        }
+    }
+
+    return( status );
+}
