721af49d9b27217827a8d29e380eb01866d46dab
david <david>
1992-07-16 10:39:18 +0000

    Initial revision

---
+++ Include/connect.h
@@ -0,0 +1,22 @@
+
+#ifndef  DEF_CONNECT
+#define  DEF_CONNECT
+
+#include  <def_standard.h>
+
+typedef  struct
+{
+    Boolean  inside;
+    int      label;
+    Boolean  queued;
+    int      dist_transform;
+    int      dist_from_region;
+    int      cutoff;
+
+    Boolean  on_path;
+    int      path_dist_transform;
+} pixel_struct;
+
+#define  INVALID_LABEL   0
+
+#endif
---
+++ Include/surface_fitting.h
@@ -0,0 +1,25 @@
+
+#ifndef  DEF_SURFACE_FITTING
+#define  DEF_SURFACE_FITTING
+
+#include  <def_standard.h>
+#include  <def_geometry.h>
+#include  <def_surface_rep.h>
+
+typedef  struct
+{
+    double              *parameters;
+    double              *descriptors;
+    int                 n_samples;
+    int                 n_surface_points;
+    Point               *surface_points;
+    Real                *surface_point_distances;
+    Real                gradient_strength_factor;
+    Real                curvature_factor;
+    Real                surface_point_distance_factor;
+    Real                surface_point_distance_threshold;
+    surface_rep_struct  *surface_representation;
+} surface_fitting_struct;
+
+
+#endif
