1cd60096fac05c1f5d396860699c1a8f39d1e8ec
david <david>
1992-07-22 11:18:14 +0000

    *** empty log message ***

--- voxel_scan/minimization.c
+++ voxel_scan/minimization.c
@@ -2,12 +2,16 @@
 #include  <def_globals.h>
 #include  <def_files.h>
 #include  <def_minimization.h>
+#include  <def_surface_rep.h>
+#include  <def_alloc.h>
+
+#define  DEBUG
 
 public  Status  apply_simplex_minimization( n_parameters, parameters,
                                        evaluate_fit_function, evaluation_ptr )
     int             n_parameters;
     double          parameters[];
-    double          (*evaluate_fit_function)();
+    double          (*evaluate_fit_function)( void *, double [] );
     void            *evaluation_ptr;
 {
     Status                   status;
@@ -31,48 +35,110 @@ public  Status  apply_simplex_minimization( n_parameters, parameters,
     return( status );
 }
 
-public  void  apply_one_parameter_minimization( max_iterations,
+public  void  apply_one_parameter_minimization( surface_rep,
+                                                descriptors,
+                                                max_iterations,
                                                 tolerance,
                                                 n_parameters, parameters,
                                                 max_parameter_deltas,
                                                 parameter_deltas,
+                                                evaluate_distances_function,
                                                 evaluate_fit_function,
                                                 evaluation_ptr )
-    int             max_iterations;
-    double          tolerance;
-    int             n_parameters;
-    double          parameters[];
-    double          max_parameter_deltas[];
-    double          parameter_deltas[];
-    double          (*evaluate_fit_function)();
-    void            *evaluation_ptr;
+    surface_rep_struct  *surface_rep;
+    double              descriptors[];
+    int                 max_iterations;
+    double              tolerance;
+    int                 n_parameters;
+    double              parameters[];
+    double              max_parameter_deltas[];
+    double              parameter_deltas[];
+    void                (*evaluate_distances_function)
+                                   ( void *, double [], Real [],
+                                      double, double, double, double );
+    double              (*evaluate_fit_function)( void *, double [],
+                                     double, double, double, double,
+                                     Real [] );
+    void                *evaluation_ptr;
 {
-    double  prev_fit, fit, prev_parameter;
-    int     n_iterations, current_parameter;
-    void    one_parameter_minimization();
+    Status   status;
+    double   prev_fit, fit, prev_parameter;
+    int      n_iterations, current_parameter;
+    void     one_parameter_minimization();
+    Real     *distances_without_this_parameter;
+    double   u_min, u_max, v_min, v_max, gain, total_gain;
 
-    fit = (*evaluate_fit_function) ( evaluation_ptr, parameters );
+    ALLOC( status, distances_without_this_parameter, n_parameters );
+
     n_iterations = 0;
+
+    fit = (*evaluate_fit_function) ( evaluation_ptr, parameters,
+                                     0.0, 1.0, 0.0, 1.0, (Real *) 0 );
 
     do
     {
         prev_fit = fit;
+        total_gain = 0.0;
 
         for_less( current_parameter, 0, n_parameters )
         {
+#ifdef DEBUG
+double  t1, t2;
+#endif
+            surface_rep->get_parameter_influence( current_parameter,
+                            descriptors, &u_min, &u_max, &v_min, &v_max );
+
+            (*evaluate_distances_function)( evaluation_ptr, parameters,
+                                            distances_without_this_parameter,
+                                            u_min, u_max, v_min, v_max );
+
             prev_parameter = parameters[current_parameter];
-            one_parameter_minimization( &fit, parameters, current_parameter,
+
+#ifdef DEBUG
+t1 = (*evaluate_fit_function) ( evaluation_ptr, parameters,
+                                         0.0, 1.0, 0.0, 1.0, (Real *) 0 );
+#endif
+
+            one_parameter_minimization( parameters, current_parameter,
+                                        u_min, u_max, v_min, v_max,
                                         max_parameter_deltas[current_parameter],
                                         &parameter_deltas[current_parameter],
-                                        evaluate_fit_function, evaluation_ptr );
+                                        distances_without_this_parameter,
+                                        evaluate_fit_function, evaluation_ptr,
+                                        &gain );
 
-            PRINT( "%g:  parameter[%d]:  %g -> %g\n",
-                   fit, current_parameter + 1,
-                   prev_parameter, parameters[current_parameter] );
+            total_gain = total_gain + gain;
+
+#ifdef DEBUG
+t2 = (*evaluate_fit_function) ( evaluation_ptr, parameters,
+                                         0.0, 1.0, 0.0, 1.0, (Real *) 0 );
+#endif
+
+            PRINT( "Parameter[%d]:  %g -> %g    = gain of %g\n",
+                   current_parameter + 1,
+                   prev_parameter, parameters[current_parameter], gain );
+
+#ifdef DEBUG
+            if( !numerically_close( t1 - t2, gain, 1.0e-2 ) )
+                PRINT( "Error  t1 %g, t2 %g, t1 - t2 %g, gain %g\n",
+                       t1, t2, t1 - t2, gain );
+#endif
         }
 
         ++n_iterations;
+
+        fit = (*evaluate_fit_function) ( evaluation_ptr, parameters,
+                                         0.0, 1.0, 0.0, 1.0, (Real *) 0 );
+
+        PRINT( "Fit has improved from %g to %g\n", prev_fit, fit );
+
+        if( !numerically_close( prev_fit - total_gain, fit, 1.0e-3 ) )
+            PRINT( "------------------- Error  total gain = %g, actual = %g\n",
+                       total_gain, prev_fit - fit );
     }
     while( n_iterations < max_iterations &&
            (n_iterations == 1 || !numerically_close(fit,prev_fit,tolerance)) );
+
+    if( status == OK )
+        FREE( status, distances_without_this_parameter );
 }
--- voxel_scan/one_parm_minimization.c
+++ voxel_scan/one_parm_minimization.c
@@ -1,19 +1,26 @@
 
 #include  <def_mni.h>
 
-public  void  one_parameter_minimization( current_fit,
-                                          parameters, which_parameter, 
+public  void  one_parameter_minimization( parameters, which_parameter,
+                                          u_min, u_max, v_min, v_max,
                                           max_delta_parameter,
                                           delta_parameter,
+                                          distances_without_this_parameter,
                                           evaluate_fit_function,
-                                          evaluation_ptr )
-    double          *current_fit;
+                                          evaluation_ptr, fitting_gain )
     double          parameters[];
     int             which_parameter;
+    double          u_min;
+    double          u_max;
+    double          v_min;
+    double          v_max;
     double          max_delta_parameter;
     double          *delta_parameter;
-    double          (*evaluate_fit_function)();
+    Real            distances_without_this_parameter[];
+    double          (*evaluate_fit_function)( void *, double [],
+                                  double, double, double, double, Real [] );
     void            *evaluation_ptr;
+    double          *fitting_gain;
 {
     const    double  TOLERANCE   = 1.0e-4;
     Boolean  done, halved;
@@ -22,7 +29,9 @@ public  void  one_parameter_minimization( current_fit,
 
     parameter_value = parameters[which_parameter];
 
-    fit_here = *current_fit;
+    fit_here = (*evaluate_fit_function) ( evaluation_ptr, parameters,
+                                u_min, u_max, v_min, v_max,
+                                distances_without_this_parameter );
 
     done = FALSE;
     halved = FALSE;
@@ -30,10 +39,14 @@ public  void  one_parameter_minimization( current_fit,
     while( !done )
     {
         parameters[which_parameter] = parameter_value - *delta_parameter;
-        fit_before = (*evaluate_fit_function) ( evaluation_ptr, parameters );
+        fit_before = (*evaluate_fit_function) ( evaluation_ptr, parameters,
+                                u_min, u_max, v_min, v_max,
+                                distances_without_this_parameter );
 
         parameters[which_parameter] = parameter_value + *delta_parameter;
-        fit_after = (*evaluate_fit_function) ( evaluation_ptr, parameters );
+        fit_after = (*evaluate_fit_function) ( evaluation_ptr, parameters,
+                                u_min, u_max, v_min, v_max,
+                                distances_without_this_parameter );
 
         if( fit_after < fit_here || fit_before < fit_here )
         {
@@ -61,13 +74,17 @@ public  void  one_parameter_minimization( current_fit,
         {
             parameters[which_parameter] = parameter_value -
                                           *delta_parameter * 2.0;
-            next_fit_before = (*evaluate_fit_function)
-                                              ( evaluation_ptr, parameters );
+            next_fit_before = (*evaluate_fit_function) 
+                              ( evaluation_ptr, parameters,
+                                u_min, u_max, v_min, v_max,
+                                distances_without_this_parameter );
 
             parameters[which_parameter] = parameter_value +
                                           *delta_parameter * 2.0;
-            next_fit_after = (*evaluate_fit_function)
-                                              ( evaluation_ptr, parameters );
+            next_fit_after = (*evaluate_fit_function) 
+                              ( evaluation_ptr, parameters,
+                                u_min, u_max, v_min, v_max,
+                                distances_without_this_parameter );
 
             if( next_fit_after >= fit_here && next_fit_before >= fit_here )
                 done = TRUE;
@@ -86,15 +103,16 @@ public  void  one_parameter_minimization( current_fit,
     if( fit_after < fit_before && fit_after < fit_here )
     {
         parameters[which_parameter] = parameter_value + *delta_parameter;
-        *current_fit = fit_after;
+        *fitting_gain = fit_here - fit_after;
     }
     else if( fit_before < fit_after && fit_before < fit_here )
     {
         parameters[which_parameter] = parameter_value - *delta_parameter;
-        *current_fit = fit_before;
+        *fitting_gain = fit_here - fit_before;
     }
     else
     {
         parameters[which_parameter] = parameter_value;
+        *fitting_gain = 0.0;
     }
 }
