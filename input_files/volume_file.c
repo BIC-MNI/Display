acbfcc75861f421f76c1ac16c86e2ac131c0a662
david <david>
1992-11-24 11:01:05 +0000

    vi !$

--- input_files/volume_file.c
+++ input_files/volume_file.c
@@ -23,7 +23,7 @@ public  Status   input_volume_file( filename, n_objects, object_list )
         volume->sizes[Y] = 0;
         volume->sizes[Z] = 0;
         (void) strcpy( volume->filename, filename );
-        volume->roi_filename[0] = (char) 0;
+        volume->activity_filename[0] = (char) 0;
         volume->axis_map[X] = X;
         volume->axis_map[Y] = Y;
         volume->axis_map[Z] = Z;
