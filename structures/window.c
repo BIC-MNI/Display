987518ab6c95c24fced0e8efe5b758dbdc5b543d
david <david>
1993-03-02 20:29:55 +0000

    Initial revision

---
+++ structures/window.c
@@ -0,0 +1,11 @@
+#include <def_graphics.h>
+
+public  void  transform_screen_to_pixels( window, screen, pixels )
+    window_struct  *window;
+    Point          *screen;
+    Point          *pixels;
+{
+    Point_x(*pixels) = (Real) (window->x_size - 1) * Point_x(*screen) + 0.5;
+    Point_y(*pixels) = (Real) (window->y_size - 1) * Point_y(*screen) + 0.5;
+    Point_z(*pixels) = 0.0;
+}
