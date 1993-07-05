55286849225cf4725f2fba4852a1441a152d6a3b
david <david>
1993-07-05 10:53:11 +0000

    *** empty log message ***

--- input_files/volume_file.c
+++ input_files/volume_file.c
@@ -2,31 +2,22 @@
 
 public  Status   input_volume_file(
     char           filename[],
-    volume_struct  *volume )
+    Volume         *volume )
 {
     Status         status;
 
-/*
-    (*volume)->sizes[X] = 0;
-    (*volume)->sizes[Y] = 0;
-    (*volume)->sizes[Z] = 0;
-    (*volume)->activity_filename[0] = (char) 0;
-    (*volume)->activity_threshold = 0.0;
-    (*volume)->axis_map[X] = X;
-    (*volume)->axis_map[Y] = Y;
-    (*volume)->axis_map[Z] = Z;
-    (*volume)->axis_flip[X] = FALSE;
-    (*volume)->axis_flip[Y] = FALSE;
-    (*volume)->axis_flip[Z] = FALSE;
-    (*volume)->slice_thickness[X] = 1.0;
-    (*volume)->slice_thickness[Y] = 1.0;
-    (*volume)->slice_thickness[Z] = 1.0;
-*/
-
     status = input_volume( filename, volume );
 
+    if( get_volume_n_dimensions( *volume ) != N_DIMENSIONS )
+    {
+        print( "Volume %s has %d dimensions, should have %d\n",
+               filename, get_volume_n_dimensions(*volume), N_DIMENSIONS );
+        delete_volume( *volume );
+        status = ERROR;
+    }
+
     if( status == OK )
-        alloc_auxiliary_data( volume );
+        alloc_auxiliary_data( *volume );
 
     return( status );
 }
