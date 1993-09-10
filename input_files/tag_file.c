cc44d6a631fce72ce1d6425a02560875aab2cff2
david <david>
1993-09-10 10:50:09 +0000

    Initial revision

---
+++ input_files/tag_file.c
@@ -0,0 +1,69 @@
+#include  <def_display.h>
+
+public  Status   input_tag_file(
+    char           filename[],
+    Colour         marker_colour,
+    Real           default_size,
+    Marker_types   default_type,
+    int            *n_objects,
+    object_struct  **object_list[] )
+{
+    Status             status;
+    object_struct      *object;
+    marker_struct      *marker;
+    FILE               *file;
+    int                i, n_volumes, n_tag_points, *structure_ids, *patient_ids;
+    char               **labels;
+    double             *weights;
+    double             **tags1, **tags2;
+
+    status = open_file_with_default_suffix( filename, "tag", READ_FILE,
+                                            ASCII_FORMAT, &file );
+
+    if( status == OK )
+    {
+        status = input_tag_points( file, &n_volumes, &n_tag_points,
+                                   &tags1, &tags2, &weights,
+                                   &structure_ids, &patient_ids,
+                                   &labels );
+    }
+
+    if( status == OK )
+    {
+        for_less( i, 0, n_tag_points )
+        {
+            object = create_object( MARKER );
+            marker = get_marker_ptr( object );
+            fill_Point( marker->position, tags1[i][X], tags1[i][Y],tags1[i][Z]);
+            (void) strcpy( marker->label, labels[i] );
+
+            if( structure_ids[i] >= 0 )
+                marker->structure_id = structure_ids[i];
+            else
+                marker->structure_id = -1;
+
+            if( patient_ids[i] >= 0 )
+                marker->patient_id = patient_ids[i];
+            else
+                marker->patient_id = -1;
+
+            if( weights[i] > 0.0 )
+                marker->size = weights[i];
+            else
+                marker->size = default_size;
+
+            marker->colour = marker_colour;
+            marker->type = default_type;
+
+            add_object_to_list( n_objects, object_list, object );
+        }
+
+        free_tag_points( n_volumes, n_tag_points, tags1, tags2, weights,
+                         structure_ids, patient_ids, labels );
+    }
+
+    if( status == OK )
+        status = close_file( file );
+
+    return( status );
+}
