653bb67678fe8520f09a7bc16f0d13262109f80e
david <david>
1994-09-20 17:10:07 +0000

    Initial revision

---
+++ Include/display.h
@@ -0,0 +1,13 @@
+#ifndef  DEF_DISPLAY
+#define  DEF_DISPLAY
+
+#include  <deform.h>
+#include  <globals.h>
+#include  <minimization.h>
+#include  <connect.h>
+#include  <main.h>
+#include  <marching_cubes.h>
+#include  <module.h>
+#include  <display_prototypes.h>
+
+#endif
