e3570dc4e63ddd1918e98ba225746997ed9c4a30
david <david>
1994-09-22 12:04:17 +0000

    *** empty log message ***

--- segmenting/fill_3d.c
+++ segmenting/fill_3d.c
@@ -9,7 +9,9 @@ public  void  fill_connected_voxels_3d(
     Volume          volume,
     Volume          label_volume,
     int             voxel[],
-    int             label,
+    int             min_label_threshold,
+    int             max_label_threshold,
+    int             desired_label,
     Real            min_threshold,
     Real            max_threshold )
 {
@@ -18,33 +20,32 @@ public  void  fill_connected_voxels_3d(
     int                          voxel_index[MAX_DIMENSIONS];
     xyz_struct                   entry;
     QUEUE_STRUCT( xyz_struct )   queue;
-    bitlist_3d_struct            bitlist;
+    bitlist_3d_struct            checked_flags, change_flags;
     int                          n_done;
     Real                         val;
     const  Real                  update_every = 10.0;
     Real                         next_message_time;
 
+    if( !should_change_this_one( volume, label_volume, voxel,
+                                 min_threshold, max_threshold,
+                                 min_label_threshold, max_label_threshold,
+                                 desired_label ) )
+        return;
+
     get_volume_sizes( volume, sizes );
 
-    create_bitlist_3d( sizes[X], sizes[Y], sizes[Z], &bitlist );
+    create_bitlist_3d( sizes[X], sizes[Y], sizes[Z], &checked_flags );
+    create_bitlist_3d( sizes[X], sizes[Y], sizes[Z], &change_flags );
 
     INITIALIZE_QUEUE( queue );
 
-    set_bitlist_bit_3d( &bitlist, voxel[X], voxel[Y], voxel[Z], TRUE );
-    if( get_volume_label_data( label_volume, voxel ) != label )
-    {
-        GET_VALUE_3D( val, volume, voxel[X], voxel[Y], voxel[Z] );
-        if( min_threshold > max_threshold ||
-            val >= min_threshold && val <= max_threshold )
-        {
-            set_volume_label_data( label_volume, voxel, label );
+    set_bitlist_bit_3d( &checked_flags, voxel[X], voxel[Y], voxel[Z], TRUE );
+    set_bitlist_bit_3d( &change_flags, voxel[X], voxel[Y], voxel[Z], TRUE );
 
-            entry.x = voxel[X];
-            entry.y = voxel[Y];
-            entry.z = voxel[Z];
-            INSERT_IN_QUEUE( queue, entry );
-        }
-    }
+    entry.x = voxel[X];
+    entry.y = voxel[Y];
+    entry.z = voxel[Z];
+    INSERT_IN_QUEUE( queue, entry );
 
     n_done = 0;
     next_message_time = current_realtime_seconds() + update_every;
@@ -69,25 +70,23 @@ public  void  fill_connected_voxels_3d(
             if( tx >= 0 && tx < sizes[X] &&
                 ty >= 0 && ty < sizes[Y] &&
                 tz >= 0 && tz < sizes[Z] &&
-                !get_bitlist_bit_3d( &bitlist, tx, ty, tz ) )
+                !get_bitlist_bit_3d( &checked_flags, tx, ty, tz ) )
             {
-                set_bitlist_bit_3d( &bitlist, tx, ty, tz, TRUE );
+                set_bitlist_bit_3d( &checked_flags, tx, ty, tz, TRUE );
 
                 voxel_index[X] = tx;
                 voxel_index[Y] = ty;
                 voxel_index[Z] = tz;
-                if( get_volume_label_data( label_volume, voxel_index ) != label)
+                if( should_change_this_one( volume, label_volume, voxel_index,
+                                 min_threshold, max_threshold,
+                                 min_label_threshold, max_label_threshold,
+                                 desired_label ) )
                 {
-                    GET_VALUE_3D( val, volume, tx, ty, tz );
-                    if( min_threshold > max_threshold ||
-                        val >= min_threshold && val <= max_threshold )
-                    {
-                        set_volume_label_data( label_volume, voxel_index,label);
-                        entry.x = tx;
-                        entry.y = ty;
-                        entry.z = tz;
-                        INSERT_IN_QUEUE( queue, entry );
-                    }
+                    set_bitlist_bit_3d( &change_flags, tx, ty, tz, TRUE );
+                    entry.x = tx;
+                    entry.y = ty;
+                    entry.z = tz;
+                    INSERT_IN_QUEUE( queue, entry );
                 }
             }
         }
@@ -100,7 +99,19 @@ public  void  fill_connected_voxels_3d(
         }
     }
 
-    delete_bitlist_3d( &bitlist );
+    for_less( voxel_index[X], 0, sizes[X] )
+    for_less( voxel_index[Y], 0, sizes[Y] )
+    for_less( voxel_index[Z], 0, sizes[Z] )
+    {
+        if( get_bitlist_bit_3d( &change_flags, voxel_index[X], voxel_index[Y],
+                                voxel_index[Z] ) )
+        {
+            set_volume_label_data( label_volume, voxel_index, desired_label );
+        }
+    }
+
+    delete_bitlist_3d( &checked_flags );
+    delete_bitlist_3d( &change_flags );
 
     DELETE_QUEUE( queue );
 }
