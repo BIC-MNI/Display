afb9c4a42d5b2d72ca89070adc606b7c11f91986
david <david>
1995-09-19 18:24:07 +0000

    check_in_all

--- callbacks/surface_extract.c
+++ callbacks/surface_extract.c
@@ -13,7 +13,7 @@
 ---------------------------------------------------------------------------- */
 
 #ifndef lint
-static char rcsid[] = "$Header: /private-cvsroot/visualization/Display/callbacks/surface_extract.c,v 1.25 1995-07-31 19:53:53 david Exp $";
+static char rcsid[] = "$Header: /private-cvsroot/visualization/Display/callbacks/surface_extract.c,v 1.26 1995-09-19 18:24:07 david Exp $";
 #endif
 
 
