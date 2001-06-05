097514663410c59a9c2a3625e480dc57bee97b7b
neelin <neelin>
2001-06-05 15:32:52 +0000

    When saving labels to a minc file in function save_label_data, copy header info from backup file rather than from original label file since frequently that file is being overwritten and under some circumstances an error will occur.

--- callbacks/segmenting.c
+++ callbacks/segmenting.c
@@ -13,7 +13,7 @@
 ---------------------------------------------------------------------------- */
 
 #ifndef lint
-static char rcsid[] = "$Header: /private-cvsroot/visualization/Display/callbacks/segmenting.c,v 1.57 2001-05-27 00:19:39 stever Exp $";
+static char rcsid[] = "$Header: /private-cvsroot/visualization/Display/callbacks/segmenting.c,v 1.58 2001-06-05 15:32:52 neelin Exp $";
 #endif
 
 
