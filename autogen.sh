e409150e72d36d18077ac3bcae8d2b72130b39ea
bert <bert>
2004-06-28 21:08:23 +0000

    Require libraries/mni-acmacros be checked out, use libtoolize

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,23 @@
 #! /bin/sh
 
-set -e
+if [ ! -r m4/mni_REQUIRE_LIB.m4 ]; then
+    cat <<EOF
+The required m4 files were not found.
+You need to check these out from their repository
+using
 
+    cvs -d /software/source checkout -d m4 libraries/mni-acmacros
+
+(yes, two '-d' options)
+Then re-run autogen.sh.
+EOF
+    exit 1
+fi
+
+set -e
 aclocal -I m4
 autoheader
+libtoolize --automake
 automake --add-missing
 autoconf
 
