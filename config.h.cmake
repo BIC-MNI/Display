c089da927e4400e23e6d1cc81255179be6a3803e
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-03-07 00:29:02 -0500

    Automate-the-version-string-based-on-git

---
+++ config.h.cmake
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* the configured options and settings for Display */
+#cmakedefine PROJECT_NAME "@PROJECT_NAME@"
+#define PROJECT_VERSION "@PROJECT_VERSION@"
+
+#cmakedefine USE_MINC2
+#cmakedefine HARD_CODED_DISPLAY_DIRECTORY1 "@HARD_CODED_DISPLAY_DIRECTORY1@"
+#cmakedefine HARD_CODED_DISPLAY_DIRECTORY2 "@HARD_CODED_DISPLAY_DIRECTORY2@"
+
+#endif // CONFIG_H
--- main/main.c
+++ main/main.c
@@ -16,9 +16,10 @@
 static char rcsid[] = "$Header: /private-cvsroot/visualization/Display/main/main.c,v 1.65 2010-10-29 19:07:52 jgsled Exp $";
 #endif
 
+#include "config.h"
 #include  <display.h>
 
-private  STRING   version = "1.5.1 Nov, 2011";
+//private  STRING   version = "1.5.1 Nov, 2011";
 
 #define  MAX_TITLE_LENGTH   200
 
@@ -32,7 +33,7 @@ private  void      initialize_view_to_fit(
 
 /* The first directory is set using compiler flag -D */
 /*#define  HARD_CODED_DISPLAY_DIRECTORY1    "/usr/local/mni/lib"*/
-#define  HARD_CODED_DISPLAY_DIRECTORY2    "/usr/local/lib"
+/*#define  HARD_CODED_DISPLAY_DIRECTORY2    "/usr/local/lib"*/
 #define  GLOBALS_FILENAME                 "Display.globals"
 #define  MENU_FILENAME                    "Display.menu"
 
@@ -173,7 +174,7 @@ int  main(
     {
         if( equal_strings( filename, "-version" ) )
         {
-            print( "%s:  Version: %s\n", argv[0], version );
+            print( "%s %s\n", PROJECT_NAME,	PROJECT_VERSION );
             return( 0 );
         }
         else if( equal_strings( filename, "-label" ) )
