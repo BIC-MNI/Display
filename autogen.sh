3cbb8ea2537e853b08f5a16c22cf1893de61b011
Robert D. Vincent <robert.d.vincent@mcgill.ca>
2015-04-27 14:40:35 -0400

    Streamline autoconf scripts.

--- autogen.sh
+++ autogen.sh
@@ -1,23 +1,9 @@
 #! /bin/sh
 
-if [ ! -r m4/mni_REQUIRE_LIB.m4 ]; then
-    cat <<EOF
-The required m4 files were not found.
-You need to check these out from their repository
-using
-
-    cvs -d /software/source checkout -d m4 libraries/mni-acmacros
-
-(yes, two '-d' options)
-Then re-run autogen.sh.
-EOF
-    exit 1
-fi
-
 set -e
 
 libtoolize --automake --copy
-aclocal -I m4
+aclocal
 autoheader
 automake --add-missing --copy
 autoconf
