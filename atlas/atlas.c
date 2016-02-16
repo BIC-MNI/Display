054053f74d61cd5561dbc26255f0a19f89c5d84f
Robert D. Vincent <robert.d.vincent@mcgill.ca>
2016-02-16 15:41:55 -0500

    Major reworking and documentation of the atlas code to get it working again.

--- atlas/atlas.c
+++ atlas/atlas.c
@@ -26,12 +26,30 @@ static  VIO_Status  input_pixel_map(
     VIO_STR          image_filename,
     pixels_struct   *pixels );
 
-static  const  int   ATLAS_SIZE[VIO_N_DIMENSIONS] = { 256, 256, 80 };
-static  const  VIO_Real  ATLAS_STEPS[VIO_N_DIMENSIONS] = { 0.67, 0.86, 1.5 };
-static  const  VIO_Real  ATLAS_STARTS[VIO_N_DIMENSIONS] = {  -86.095, -126.51, -37.5 };
+/** Lengths of the sides of the atlas volume. */
+static const int ATLAS_SIZE[VIO_N_DIMENSIONS] = {
+  256, 256, 80
+};
 
-  void  initialize_atlas(
-    atlas_struct   *atlas )
+/** Step sizes of the atlas volume. */
+static const VIO_Real ATLAS_STEPS[VIO_N_DIMENSIONS] = {
+  0.67, 0.86, 1.5
+};
+
+/** Start positions of the atlas volume. */
+static const VIO_Real ATLAS_STARTS[VIO_N_DIMENSIONS] = {
+  -86.095, -126.51, -37.5
+};
+
+/**
+ * \brief Initialize the atlas for the slice view window.
+ *
+ * Sets up the basic parameters and information but does not
+ * actually load the atlas.
+ *
+ * \param atlas The atlas_struct to be initialized.
+ */
+void  initialize_atlas( atlas_struct *atlas )
 {
     atlas->input = FALSE;
     atlas->enabled = FALSE;
@@ -46,8 +64,13 @@ static  const  VIO_Real  ATLAS_STARTS[VIO_N_DIMENSIONS] = {  -86.095, -126.51, -
     atlas->n_images = 0;
 }
 
-  void  delete_atlas(
-    atlas_struct   *atlas )
+
+/**
+ * \brief Remove the atlas from memory.
+ *
+ * \param atlas The atlas_struct to be removed.
+ */
+void  delete_atlas( atlas_struct *atlas )
 {
     int    im;
 
@@ -62,56 +85,75 @@ static  const  VIO_Real  ATLAS_STARTS[VIO_N_DIMENSIONS] = {  -86.095, -126.51, -
     }
 }
 
+/**
+ * \brief Convert a pixels object to a 3D volume.
+ *
+ * This function is private to the atlas and isn't really generic in
+ * any way. The volume is nominally 3D, but it will have a singleton
+ * dimension along the slice axis.
+ *
+ * \param axis_index The axis index associated with this pixels object.
+ * \param slice_position The position of this pixels object along the selected axis.
+ * \param pixels The pixels_struct to be converted to a volume.
+ *
+ * \returns The newly-created volume.
+ */
 static  VIO_Volume  convert_pixels_to_volume(
     int            axis_index,
-    VIO_Real           slice_position,
+    VIO_Real       slice_position,
     pixels_struct  *pixels )
 {
-    int      x, y, sizes[2];
-    int      ind, dim, x_index, y_index;
-    VIO_STR   dim_names[2];
-    int      dim_orders[VIO_N_DIMENSIONS][2] = {
-                                                         { VIO_Y, VIO_Z },
-                                                         { VIO_X, VIO_Z },
-                                                         { VIO_X, VIO_Y }
-                                                      };
+    int      x, y, sizes[VIO_N_DIMENSIONS];
+    int      dim, x_index, y_index;
+    VIO_STR  dim_names[VIO_N_DIMENSIONS];
+    int      dim_orders[VIO_N_DIMENSIONS][VIO_N_DIMENSIONS] = {
+      { VIO_Y, VIO_Z, VIO_X },
+      { VIO_X, VIO_Z, VIO_Y },
+      { VIO_X, VIO_Y, VIO_Z }
+    };
     VIO_Volume   volume;
-    VIO_Real     separations[2];
-    VIO_Real     bottom_left[2];
+    VIO_Real     separations[VIO_N_DIMENSIONS];
+    VIO_Real     bottom_left[VIO_N_DIMENSIONS];
     VIO_Real     world_corner[VIO_N_DIMENSIONS];
 
-    for_less( dim, 0, 2 )
+    for_less( dim, 0, VIO_N_DIMENSIONS )
         dim_names[dim] = XYZ_dimension_names[dim_orders[axis_index][dim]];
 
-    volume = create_volume( 2, dim_names, NC_BYTE, FALSE, 0.0, 0.0 );
+    volume = create_volume( VIO_N_DIMENSIONS, dim_names, NC_BYTE, FALSE,
+                            0.0, 0.0 );
 
     set_volume_real_range( volume, 0.0, 255.0 );
 
-    sizes[0] = pixels->x_size;
-    sizes[1] = pixels->y_size;
+    sizes[VIO_X] = pixels->x_size;
+    sizes[VIO_Y] = pixels->y_size;
+    sizes[VIO_Z] = 1;
 
     set_volume_sizes( volume, sizes );
 
     alloc_volume_data( volume );
 
     for_less( x, 0, pixels->x_size )
-    for_less( y, 0, pixels->y_size )
     {
-        ind = (int) PIXEL_COLOUR_INDEX_8( *pixels, x, y );
+        for_less( y, 0, pixels->y_size )
+        {
+            int ind = (int) PIXEL_COLOUR_INDEX_8( *pixels, x, y );
 
-        set_volume_voxel_value( volume, x, y, 0, 0, 0, (VIO_Real) ind );
+            set_volume_voxel_value( volume, x, y, 0, 0, 0, (VIO_Real) ind );
+        }
     }
 
-    x_index = dim_orders[axis_index][0];
-    y_index = dim_orders[axis_index][1];
+    x_index = dim_orders[axis_index][VIO_X];
+    y_index = dim_orders[axis_index][VIO_Y];
 
-    separations[0] = ATLAS_STEPS[x_index] * (VIO_Real) ATLAS_SIZE[x_index] /
-                     (VIO_Real) pixels->x_size;
-    separations[1] = ATLAS_STEPS[y_index] * (VIO_Real) ATLAS_SIZE[y_index] /
-                     (VIO_Real) pixels->y_size;
+    separations[VIO_X] = ATLAS_STEPS[x_index] * ATLAS_SIZE[x_index] /
+                         pixels->x_size;
+    separations[VIO_Y] = ATLAS_STEPS[y_index] * ATLAS_SIZE[y_index] /
+                         pixels->y_size;
+    separations[VIO_Z] = ATLAS_STEPS[axis_index];
 
-    bottom_left[0] = -0.5;
-    bottom_left[1] = -0.5;
+    bottom_left[VIO_X] = -0.5;
+    bottom_left[VIO_Y] = -0.5;
+    bottom_left[VIO_Z] = -0.5;
 
     world_corner[x_index] = ATLAS_STARTS[x_index] - ATLAS_STEPS[x_index] / 2.0;
     world_corner[y_index] = ATLAS_STARTS[y_index] - ATLAS_STEPS[y_index] / 2.0;
@@ -123,17 +165,33 @@ static  VIO_Volume  convert_pixels_to_volume(
     return( volume );
 }
 
+/**
+ * \brief Input the atlas from the file system.
+ *
+ * Loads the individual atlas slices from the file system. The
+ * given filename should be a text file where each line has the
+ * format:
+ * 
+ * \code <slice-filename> [x|y|z] <coordinate> \endcode
+ * 
+ * Each individual slice contains a single pixels object that corresponds
+ * to the atlas image for the x, y, or z axis at the given numeric coordinate.
+ *
+ * \param atlas The atlas_struct into which the slices will be loaded.
+ * \param filename The filename of the text file listing all of the slices.
+ * \returns VIO_OK if all goes well.
+ */
 static  VIO_Status  input_atlas(
     atlas_struct   *atlas,
     VIO_STR         filename )
 {
-    VIO_Status           status;
+    VIO_Status       status;
     FILE             *file;
-    VIO_STR           *image_filenames, image_filename;
+    VIO_STR          *image_filenames, image_filename;
     char             axis_letter;
-    VIO_Real             talairach_position;
+    VIO_Real         talairach_position;
     int              axis_index, image;
-    VIO_STR           atlas_directory;
+    VIO_STR          atlas_directory;
     pixels_struct    pixels;
     VIO_progress_struct  progress;
 
@@ -226,17 +284,25 @@ static  VIO_Status  input_atlas(
     return( status );
 }
 
+/**
+ * \brief Read a pixel map for the atlas.
+ *
+ * \param default_directory The directory for the atlas slice file.
+ * \param image_filename The individual filename of the atlas slice file.
+ * \param pixels The pixels_struct to be filled in with the image.
+ * \returns VIO_OK if all goes well.
+ */
 static  VIO_Status  input_pixel_map(
     VIO_STR         default_directory,
     VIO_STR         image_filename,
     pixels_struct  *pixels )
 {
-    VIO_Status         status;
-    VIO_STR         absolute_filename;
-    VIO_File_formats   format;
-    Object_types   object_type;
-    VIO_BOOL        eof;
-    FILE           *file;
+    VIO_Status       status;
+    VIO_STR          absolute_filename;
+    VIO_File_formats format;
+    Object_types     object_type;
+    VIO_BOOL         eof;
+    FILE             *file;
 
     absolute_filename = get_absolute_filename( image_filename,
                                                default_directory );
@@ -262,12 +328,27 @@ static  VIO_Status  input_pixel_map(
     return( status );
 }
 
-  void  regenerate_atlas_lookup(
+/**
+ * \brief Rebuild the atlas lookup table.
+ *
+ * Called when changes to the atlas might justify some kind of
+ * restructuring of the atlas table. Not really necessary.
+ *
+ * \param slice_window The display_struct of the slice view window.
+ */
+void  regenerate_atlas_lookup(
     display_struct    *slice_window )
 {
 }
 
-  void  set_atlas_state(
+/**
+ * \brief Turn the atlas on or off.
+ *
+ * \param slice_window The display_struct of the slice view window.
+ * \param state a boolean flag indicating whether the atlas should be on
+ * or off.
+ */
+void  set_atlas_state(
     display_struct    *slice_window,
     VIO_BOOL           state )
 {
@@ -286,8 +367,13 @@ static  VIO_Status  input_pixel_map(
     slice_window->slice.atlas.enabled = state;
 }
 
-  VIO_BOOL  is_atlas_loaded(
-    display_struct  *display )
+/**
+ * \brief Check whether the atlas is loaded.
+ *
+ * \param display The display_struct of a window.
+ * \returns TRUE if the atlas is loaded.
+ */
+VIO_BOOL is_atlas_loaded( display_struct *display )
 {
     display_struct  *slice_window;
 
@@ -295,25 +381,41 @@ static  VIO_Status  input_pixel_map(
             slice_window->slice.atlas.n_images > 0 );
 }
 
+/**
+ * \brief Find the nearest atlas image for this axis.
+ * \param atlas The atlas_struct.
+ * \param x_n_pixels Width of the desired image.
+ * \param y_n_pixels Height of the desired image.
+ * \param world_start The current position of the volume.
+ * \param world_x_axis The current view's column axis.
+ * \param world_y_axis The current view's row axis.
+ * \param image The image volume returned.
+ * \param origin The origin of the returned atlas image.
+ * \param x_axis The column axis of the returned atlas image.
+ * \param y_axis The row axis of the returned atlas image.
+ * \param x_scale The amount to scale the atlas image.
+ * \param y_scale The amount to scale the atlas image.
+ * \returns TRUE if an appropriate image was found.
+ */
 static  VIO_BOOL  find_appropriate_atlas_image(
-    atlas_struct      *atlas,
-    int               x_n_pixels,
-    int               y_n_pixels,
-    VIO_Real              world_start[],
-    VIO_Real              world_x_axis[],
-    VIO_Real              world_y_axis[],
-    VIO_Volume            *image,
-    VIO_Real              origin[],
-    VIO_Real              x_axis[],
-    VIO_Real              y_axis[],
-    VIO_Real              *x_scale,
-    VIO_Real              *y_scale )
+    atlas_struct    *atlas,
+    int             x_n_pixels,
+    int             y_n_pixels,
+    const VIO_Real  world_start[],
+    const VIO_Real  world_x_axis[],
+    const VIO_Real  world_y_axis[],
+    VIO_Volume      *image,
+    VIO_Real        origin[],
+    VIO_Real        x_axis[],
+    VIO_Real        y_axis[],
+    VIO_Real        *x_scale,
+    VIO_Real        *y_scale )
 {
-    VIO_Real            min_dist, dist, slice_position, best_scale, scale_dist;
-    VIO_Real            separations[2];
-    VIO_Real            tmp_x_scale, tmp_y_scale, tmp_origin[2];
-    VIO_Real            tmp_x_axis[2];
-    VIO_Real            tmp_y_axis[2];
+    VIO_Real        min_dist, dist, slice_position, best_scale, scale_dist;
+    VIO_Real        separations[VIO_N_DIMENSIONS];
+    VIO_Real        tmp_x_scale, tmp_y_scale, tmp_origin[VIO_N_DIMENSIONS];
+    VIO_Real        tmp_x_axis[VIO_N_DIMENSIONS];
+    VIO_Real        tmp_y_axis[VIO_N_DIMENSIONS];
     int             im, axis, dim, a1, a2;
 
     *image = NULL;
@@ -352,31 +454,37 @@ static  VIO_BOOL  find_appropriate_atlas_image(
                 (*image == NULL || dist <= min_dist) )
             {
                 convert_world_to_voxel( atlas->images[im].image,
-                                world_start[VIO_X], world_start[VIO_Y], world_start[VIO_Z],
-                                tmp_origin );
+                                        world_start[VIO_X],
+                                        world_start[VIO_Y],
+                                        world_start[VIO_Z],
+                                        tmp_origin );
 
                 convert_world_vector_to_voxel( atlas->images[im].image,
-                                       world_x_axis[VIO_X], world_x_axis[VIO_Y],
-                                       world_x_axis[VIO_Z], tmp_x_axis );
+                                               world_x_axis[VIO_X],
+                                               world_x_axis[VIO_Y],
+                                               world_x_axis[VIO_Z],
+                                               tmp_x_axis );
 
                 convert_world_vector_to_voxel( atlas->images[im].image,
-                                       world_y_axis[VIO_X], world_y_axis[VIO_Y],
-                                       world_y_axis[VIO_Z], tmp_y_axis );
+                                               world_y_axis[VIO_X],
+                                               world_y_axis[VIO_Y],
+                                               world_y_axis[VIO_Z],
+                                               tmp_y_axis );
 
                 get_volume_separations( atlas->images[im].image, separations );
 
-                for_less( dim, 0, 2 )
+                for_less( dim, 0, VIO_N_DIMENSIONS )
                 {
                     if( tmp_x_axis[dim] != 0.0 )
                     {
                         tmp_x_scale = 1.0 / VIO_FABS( separations[dim] *
-                                                  tmp_x_axis[dim] );
+                                                      tmp_x_axis[dim] );
                         scale_dist = VIO_FABS( 1.0 / VIO_FABS(tmp_x_axis[dim]) - 1.0 );
                     }
 
                     if( tmp_y_axis[dim] != 0.0 )
                         tmp_y_scale = 1.0 / VIO_FABS( separations[dim] *
-                                                  tmp_y_axis[dim] );
+                                                      tmp_y_axis[dim] );
 
                     if( tmp_x_axis[dim] == 0.0 && tmp_y_axis[dim] == 0.0 )
                         tmp_origin[dim] = 0.0;
@@ -390,7 +498,7 @@ static  VIO_BOOL  find_appropriate_atlas_image(
                     best_scale = scale_dist;
                     *x_scale = tmp_x_scale;
                     *y_scale = tmp_y_scale;
-                    for_less( dim, 0, 2 )
+                    for_less( dim, 0, VIO_N_DIMENSIONS )
                     {
                         origin[dim] = tmp_origin[dim];
                         x_axis[dim] = tmp_x_axis[dim];
@@ -404,36 +512,48 @@ static  VIO_BOOL  find_appropriate_atlas_image(
     return( *image != NULL );
 }
 
-  VIO_BOOL  render_atlas_slice_to_pixels(
+/**
+ * \brief Render an atlas slice into a pixels object.
+ *
+ * \param atlas The atlas_struct
+ * \param image buffer for the the pixel RGB values.
+ * \param image_x_size The width of the desired image.
+ * \param image_y_size The height of the desired image.
+ * \param world_start The current position of the volume.
+ * \param world_x_axis The current view's column axis.
+ * \param world_y_axis The current view's row axis.
+ * \returns TRUE if the pixels are valid.
+ */
+VIO_BOOL  render_atlas_slice_to_pixels(
     atlas_struct  *atlas,
-    VIO_Colour        image[],
+    VIO_Colour    image[],
     int           image_x_size,
     int           image_y_size,
-    VIO_Real          world_start[],
-    VIO_Real          world_x_axis[],
-    VIO_Real          world_y_axis[] )
+    VIO_Real      world_start[],
+    VIO_Real      world_x_axis[],
+    VIO_Real      world_y_axis[] )
 {
-    int            x, y;
-    int            r_atlas, g_atlas, b_atlas, a_atlas;
-    VIO_Real           x_scale, y_scale;
-    VIO_Real           origin[2];
-    VIO_Real           x_axis[2];
-    VIO_Real           y_axis[2];
-    int            transparent_threshold;
-    int            n_alloced;
-    VIO_Colour         atlas_pixel, *lookup;
-    pixels_struct  pixels;
-    VIO_Volume         atlas_image;
-    VIO_Real           opacity;
+    int           x, y;
+    int           r_atlas, g_atlas, b_atlas, a_atlas;
+    VIO_Real      x_scale, y_scale;
+    VIO_Real      origin[VIO_N_DIMENSIONS];
+    VIO_Real      x_axis[VIO_N_DIMENSIONS];
+    VIO_Real      y_axis[VIO_N_DIMENSIONS];
+    int           transparent_threshold;
+    int           n_alloced;
+    VIO_Colour    atlas_pixel, *lookup;
+    pixels_struct pixels;
+    VIO_Volume    atlas_image;
+    VIO_Real      opacity;
 
-    if( !atlas->enabled || atlas->opacity <= 0.0 ||
-        !find_appropriate_atlas_image( atlas, image_x_size, image_y_size,
+    if( !atlas->enabled || atlas->opacity <= 0.0)
+        return FALSE;
+
+    if( !find_appropriate_atlas_image( atlas, image_x_size, image_y_size,
                                        world_start, world_x_axis, world_y_axis,
                                        &atlas_image, origin, x_axis, y_axis,
                                        &x_scale, &y_scale ) )
-    {
         return( FALSE );
-    }
 
     lookup = get_8bit_rgb_pixel_lookup();
 
@@ -460,7 +580,7 @@ static  VIO_BOOL  find_appropriate_atlas_image(
     {
         for_less( y, 0, image_y_size )
         {
-            atlas_pixel = PIXEL_RGB_COLOUR( pixels, x, y ); 
+            atlas_pixel = PIXEL_RGB_COLOUR( pixels, x, y );
 
             r_atlas = get_Colour_r(atlas_pixel);
             g_atlas = get_Colour_g(atlas_pixel);
