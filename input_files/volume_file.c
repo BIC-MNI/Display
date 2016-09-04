d9fec82a499c3b598c5c5f299df998602ce3b393
Robert D. Vincent <robert.d.vincent@mcgill.ca>
2016-09-04 10:56:41 -0400

    Correct a comment.

--- input_files/volume_file.c
+++ input_files/volume_file.c
@@ -20,7 +20,8 @@
 
 #include  <display.h>
 
-/** We limit ourselves to 4-dimensional volumes at the moment. RGB volumes
+/** We limit ourselves to 5-dimensional volumes, but don't really handle
+ * the vector_dimemsion in a useful way. RGB volumes
  * (containing a vector_dimension of length three) are a special case.
  */
 #define MAX_VOLUME_DIMENSIONS 5
